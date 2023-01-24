f3682960511ae6dbc9778e42c6bb5ed71383d0fa
Wes Mehaffy <wesonweb@gmail.com>
2023-01-24 19:27:29 +0000

    create restaurant model and controller and add faker info

--- Gemfile
+++ Gemfile
@@ -27,6 +27,11 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Faker
+gem "faker"
+
+# Simple form
+gem "simple_form", github: "heartcombo/simple_form"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database'
+Restaurant.destroy_all
+puts 'creating restaurants'
+20.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Restaurant with id: #{restaurant.id} has been created"
+end
+
+puts 'Finished!'
