863f8ad485978f38c3ebe9b9a115b4102bd805e8
Wes Mehaffy <wesonweb@gmail.com>
2023-01-25 08:07:05 +0000

    add view, form and new action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[new create]
+  def new
+    @review = Review.new
+  end
+
+  private
+
+  def set_review
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def reviews_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
